d793850904316bf05c8c9365f00e207f2f27a51a
prutson <joaopcneto3@gmail.com>
2023-08-14 18:57:42 -0300

    adicionando pagina de login

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,11 +1,27 @@
 from flask import render_template
 from app import app
-
+import datetime
+from app.models.forms import LoginForm
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', 
+                           current_time=datetime.datetime.now().year)
+
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    else:
+        print(form.errors)
+    return render_template('login.html', 
+                           current_time=datetime.datetime.now().year,
+                           form=form)
+
 
 @app.route("/test", defaults={'name': None})
 @app.route('/test/<name>')
--- config.py
+++ config.py
@@ -2,3 +2,5 @@ DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
 SQLALCHEMY_TRACK_NOTIFICATION = True
+
+SECRET_KEY = "chave-bem-secura"
